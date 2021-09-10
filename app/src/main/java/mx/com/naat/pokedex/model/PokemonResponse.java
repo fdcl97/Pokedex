9e67dee88c50c4637fcc299ea0d7c77207f13ddb
Dany <dcaballero@na-at.com.mx>
2021-09-10 12:53:34 -0500

    Pokemons obtained

---
+++ app/src/main/java/mx/com/naat/pokedex/model/PokemonResponse.java
@@ -0,0 +1,15 @@
+package mx.com.naat.pokedex.model;
+
+import java.util.ArrayList;
+
+public class PokemonResponse {
+    private ArrayList<Pokemon> results;
+
+    public ArrayList<Pokemon> getPokemons() {
+        return results;
+    }
+
+    public void setPokemons(ArrayList<Pokemon> results) {
+        this.results = results;
+    }
+}
